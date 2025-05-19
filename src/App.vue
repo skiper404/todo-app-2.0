d53b8b25665ebfdbb9e24996daeb23bb523b09e7
Kirill Stetsenko <stetsenkoki@gmail.com>
2025-05-19 19:58:40 +0300

    Init project

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex h-screen justify-center bg-black text-white">
+    Todo App 2.0
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
